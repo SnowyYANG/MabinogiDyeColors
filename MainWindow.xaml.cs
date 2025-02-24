7c4e8979ff4e6f250980357f71b6271e3902ab62
SnowyYANG <snowyyang@outlook.com>
2025-02-25 01:01:15 +0800

    basic search

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,44 @@ namespace MabinogiDyeColors
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        int i = 0;
+        int moreColorsTimes = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var searchStrings = input.Text.Split(' ');
+            Color[] pallete = new Color[] { };
+            List<Color> results = new List<Color>();
+            if (cloth.IsChecked == true) pallete = Palettes.CLOTH;
+            foreach(Color c in pallete)
+            {
+                results.Add(c);
+            }
+            foreach(var str in searchStrings)
+            {
+                if (Regex.IsMatch(str, @"[0-9a-f]{6}$"))
+                {
+                    var s = str;
+                    if (s[0] == '#') s = s.Substring(1);
+                    Color target = Color.FromRgb(byte.Parse(s.Substring(0, 2), System.Globalization.NumberStyles.HexNumber), byte.Parse(s.Substring(2, 2), System.Globalization.NumberStyles.HexNumber), byte.Parse(s.Substring(4, 2), System.Globalization.NumberStyles.HexNumber));
+                    output.Children.Add(new TextBlock() { Background = new SolidColorBrush(target), Text = "目标颜色" });
+                    results.Sort((a, b) =>
+                        (int)(2 * Math.Pow(a.R - target.R, 2) + 4 * Math.Pow(a.G - target.G, 2) + 3 * Math.Pow(a.B - target.B, 2) - 
+                        2 * Math.Pow(b.R - target.R, 2) + 4 * Math.Pow(b.G - target.G, 2) + 3 * Math.Pow(b.B - target.B, 2)));
+                }
+            }
+            if (results.Count == 0)
+            {
+                output.Children.Add(new TextBlock() { Text = "无匹配颜色" });
+            }
+            else
+            {
+                foreach (var c in results)
+                {
+                    output.Children.Add(new TextBox() { Background = new SolidColorBrush(c), Text = "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2") });
+                }
+            }
         }
     }
 }
---
+++ Palettes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MabinogiDyeColors
+{
+    static class Palettes
+    {
+        public static readonly Color[] CLOTH = new Color[] { Color.FromRgb(0, 0, 0), Color.FromRgb(178, 61, 144) };
+    }
+}
